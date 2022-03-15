ecc54f6274a14e3dbfe6b0fe5c930c478d90e8bd
Yusuke Kuoka <ykuoka@gmail.com>
2022-03-15 18:30:36 +0900

    Add a chart repo server implementation for testing (#22)
    It has been so hard to reliably integration-test chartify in CI as it depended on various real remote charts, just to ensure the code goes through helm-fetch and helm-dep-up invocations that is necessary to make sure chartify works with chart dependencies.
    
    This is an attempt to resolve that issue, by introducing an implementation of Helm chart repository server for testing-purposes. It is an HTTP server that automatically `helm-package`s the local charts, generate the index.yaml, and serve the packages and the index via HTTP.
    
    Any integration test can use this server like a standard remote chart repository server. The charts served by the server does not change except we change it ourselves, so that the test reliability issue is resolved.

---
+++ chartrepo/server.go
@@ -0,0 +1,225 @@
+// Copyright The Helm Authors
+// https://github.com/helm/chart-releaser/blob/main/pkg/releaser/releaser.go
+// Part of this source has been derived from the upstream source linked above.
+
+package chartrepo
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"helm.sh/helm/v3/pkg/chart/loader"
+	"helm.sh/helm/v3/pkg/provenance"
+	"helm.sh/helm/v3/pkg/repo"
+)
+
+type Server struct {
+	Port int
+	Host string
+}
+
+func (s *Server) Run(ctx context.Context, chartsDir string) error {
+	port := s.Port
+	if port == 0 {
+		port = 18080
+	}
+
+	hostport := s.Host
+	if hostport == "" {
+		hostport = fmt.Sprintf("localhost:%d", port)
+	}
+
+	serverURL := fmt.Sprintf("http://%s/", hostport)
+
+	worktree, err := os.MkdirTemp(os.TempDir(), "chartrepo")
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := os.RemoveAll(worktree); err != nil {
+			log.Printf("unable to remove worktree %s: %v", worktree, err)
+		}
+	}()
+
+	dirEntries, err := os.ReadDir(chartsDir)
+	if err != nil {
+		return err
+	}
+
+	for _, ent := range dirEntries {
+		if !ent.IsDir() {
+			continue
+		}
+
+		chart := filepath.Join(chartsDir, ent.Name())
+
+		log.Println("Packaging", chart)
+
+		abs, err := filepath.Abs(chart)
+		if err != nil {
+			return fmt.Errorf("unable to get abs path to %s: %w", chart, err)
+		}
+
+		cmd := exec.CommandContext(ctx, "helm", "package", abs)
+		cmd.Dir = worktree
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Println(string(out))
+			return fmt.Errorf("unable to package %s: %w", chart, err)
+		}
+	}
+
+	indexYamlPath := filepath.Join(worktree, "index.yaml")
+
+	var indexFile *repo.IndexFile
+	_, err = os.Stat(indexYamlPath)
+	if err == nil {
+		indexFile, err = repo.LoadIndexFile(indexYamlPath)
+		if err != nil {
+			return err
+		}
+	} else if errors.Is(err, os.ErrNotExist) {
+		indexFile = repo.NewIndexFile()
+	} else {
+		return err
+	}
+
+	chartPackages, err := filepath.Glob(filepath.Join(worktree, "*.tgz"))
+	if err != nil {
+		return err
+	}
+
+	var update bool
+	for _, chartPackage := range chartPackages {
+		_, err := loader.LoadFile(chartPackage)
+		if err != nil {
+			return err
+		}
+		pkgURL := fmt.Sprintf("%s%s", serverURL, filepath.Base(chartPackage))
+
+		downloadUrl, _ := url.Parse(pkgURL)
+		name := filepath.Base(downloadUrl.Path)
+		baseName := strings.TrimSuffix(name, filepath.Ext(name))
+		tagParts := splitPackageNameAndVersion(baseName)
+		packageName, packageVersion := tagParts[0], tagParts[1]
+		fmt.Printf("Found %s-%s.tgz\n", packageName, packageVersion)
+		if _, err := indexFile.Get(packageName, packageVersion); err != nil {
+			if err := addToIndexFile(worktree, indexFile, downloadUrl.String()); err != nil {
+				return err
+			}
+			update = true
+			break
+		}
+	}
+
+	if !update {
+		fmt.Printf("Index %s did not change\n", indexYamlPath)
+		return nil
+	}
+
+	fmt.Printf("Updating index %s\n", indexYamlPath)
+	indexFile.SortEntries()
+
+	indexFile.Generated = time.Now()
+
+	if err := indexFile.WriteFile(indexYamlPath, 0644); err != nil {
+		return err
+	}
+
+	var serveMux http.ServeMux
+
+	serveMux.HandleFunc("/index.yaml", func(w http.ResponseWriter, _ *http.Request) {
+		f, err := os.Open(indexYamlPath)
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		if _, err := io.Copy(w, f); err != nil {
+			w.Write([]byte(err.Error()))
+			return
+		}
+	})
+
+	serveMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if !strings.HasSuffix(r.URL.Path, ".tgz") {
+			w.WriteHeader(404)
+			return
+		}
+
+		base := filepath.Base(r.URL.Path)
+
+		pkgPath := filepath.Join(worktree, base)
+
+		f, err := os.Open(pkgPath)
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		if _, err := io.Copy(w, f); err != nil {
+			w.Write([]byte(err.Error()))
+			return
+		}
+	})
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: &serveMux,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		return fmt.Errorf("starting server: %w", err)
+	}
+
+	<-ctx.Done()
+
+	return server.Close()
+}
+
+func splitPackageNameAndVersion(pkg string) []string {
+	delimIndex := strings.LastIndex(pkg, "-")
+	return []string{pkg[0:delimIndex], pkg[delimIndex+1:]}
+}
+
+func addToIndexFile(worktree string, indexFile *repo.IndexFile, url string) error {
+	arch := filepath.Join(worktree, filepath.Base(url))
+
+	// extract chart metadata
+	fmt.Printf("Extracting chart metadata from %s\n", arch)
+	c, err := loader.LoadFile(arch)
+	if err != nil {
+		return fmt.Errorf("%s is not a helm chart package: %w", arch, err)
+	}
+	// calculate hash
+	fmt.Printf("Calculating Hash for %s\n", arch)
+	hash, err := provenance.DigestFile(arch)
+	if err != nil {
+		return err
+	}
+
+	// remove url name from url as helm's index library
+	// adds it in during .Add
+	// there should be a better way to handle this :(
+	s := strings.Split(url, "/")
+	s = s[:len(s)-1]
+
+	// Add to index
+	if err := indexFile.MustAdd(c.Metadata, filepath.Base(arch), strings.Join(s, "/"), hash); err != nil {
+		return err
+	}
+	return nil
+}
