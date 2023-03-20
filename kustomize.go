3c893757035e445620b92fc346842347695b67fe
yxxhero <aiopsclub@163.com>
2023-03-20 20:07:16 +0800

    fix kustomize version detect issue
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- kustomize.go
+++ kustomize.go
@@ -5,9 +5,8 @@ import (
 	"os"
 	"path"
 	"path/filepath"
-	"strconv"
-	"strings"
 
+	"github.com/Masterminds/semver/v3"
 	"gopkg.in/yaml.v3"
 )
 
@@ -142,26 +141,24 @@ func (r *Runner) KustomizeBuild(srcDir string, tempDir string, opts ...Kustomize
 	outputFile := filepath.Join(tempDir, "templates", "kustomized.yaml")
 	kustomizeArgs := []string{"-o", outputFile, "build"}
 
-	versionInfo, err := r.run(r.kustomizeBin(), "version", "--short")
+	versionInfo, err := r.run(r.kustomizeBin(), "version")
 	if err != nil {
 		return "", err
 	}
-	version := strings.Split(strings.TrimPrefix(versionInfo, "{kustomize/v"), ".")
-	major, err := strconv.Atoi(version[0])
+
+	version, err := semver.NewVersion(versionInfo)
 	if err != nil {
-		r.Logf("Failed to parse `kustomize version` output: %v\nFalling-back to the kustomize v4 mode...", err)
+		return "", err
 	}
 
-	if major > 3 {
+	if version.Major() > 3 {
 		kustomizeArgs = append(kustomizeArgs, "--load-restrictor=LoadRestrictionsNone")
-		if u.EnableAlphaPlugins {
-			kustomizeArgs = append(kustomizeArgs, "--enable-alpha-plugins")
-		}
 	} else {
 		kustomizeArgs = append(kustomizeArgs, "--load_restrictor=none")
-		if u.EnableAlphaPlugins {
-			kustomizeArgs = append(kustomizeArgs, "--enable_alpha_plugins")
-		}
+	}
+
+	if u.EnableAlphaPlugins {
+		kustomizeArgs = append(kustomizeArgs, "--enable-alpha-plugins")
 	}
 
 	out, err := r.runInDir(tempDir, r.kustomizeBin(), append(kustomizeArgs, tempDir)...)
