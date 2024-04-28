10b78891343dea15280bc3e18850e9ad0d2bc2d3
Clement Chevalier <clement.chevalier@contentsquare.com>
2024-04-28 20:24:59 +0200

    chore: use patches instead of deprecated patchesJson6902
    Signed-off-by: Clement Chevalier <clement.chevalier@contentsquare.com>

--- patch.go
+++ patch.go
@@ -55,7 +55,7 @@ resources:
 	}
 
 	if len(u.JsonPatches) > 0 {
-		kustomizationYamlContent += `patchesJson6902:
+		kustomizationYamlContent += `patches:
 `
 		for i, f := range u.JsonPatches {
 			fileBytes, err := r.ReadFile(f)
