f02341ab5442b662c5eb4a1a11f45fc09d3a7abd
yxxhero <aiopsclub@163.com>
2024-08-29 07:31:22 +0800

    fix tests
    Signed-off-by: yxxhero <aiopsclub@163.com>

--- go.mod
+++ go.mod
@@ -1,8 +1,6 @@
 module github.com/helmfile/chartify
 
-go 1.22.0
-
-toolchain go1.22.3
+go 1.22.6
 
 require (
 	github.com/Masterminds/semver/v3 v3.2.1
--- integration_test.go
+++ integration_test.go
@@ -28,7 +28,7 @@ func TestIntegration(t *testing.T) {
 	repo := "myrepo"
 	startServer(t, repo)
 
-	// SAVE_SNAPSHOT=1 go1.20 test -run ^TestIntegration/adhoc_dependency_condition$ ./
+	// SAVE_SNAPSHOT=1 go1.22 test -run ^TestIntegration/adhoc_dependency_condition$ ./
 	runTest(t, integrationTestCase{
 		description: "adhoc dependency condition",
 		release:     "myapp",
@@ -47,7 +47,7 @@ func TestIntegration(t *testing.T) {
 		},
 	})
 
-	// SAVE_SNAPSHOT=1 go1.20 test -run ^TestIntegration/adhoc_dependency_condition_disabled$ ./
+	// SAVE_SNAPSHOT=1 go1.22 test -run ^TestIntegration/adhoc_dependency_condition_disabled$ ./
 	runTest(t, integrationTestCase{
 		description: "adhoc dependency condition disabled",
 		release:     "myapp",
@@ -66,7 +66,7 @@ func TestIntegration(t *testing.T) {
 		},
 	})
 
-	// SAVE_SNAPSHOT=1 go1.20 test -run ^TestIntegration/adhoc_dependency_condition_default$ ./
+	// SAVE_SNAPSHOT=1 go1.22 test -run ^TestIntegration/adhoc_dependency_condition_default$ ./
 	runTest(t, integrationTestCase{
 		description: "adhoc dependency condition default",
 		release:     "myapp",
@@ -82,7 +82,7 @@ func TestIntegration(t *testing.T) {
 		},
 	})
 
-	// SAVE_SNAPSHOT=1 go1.20 test -run ^TestIntegration/force_namespace$ ./
+	// SAVE_SNAPSHOT=1 go1.22 test -run ^TestIntegration/force_namespace$ ./
 	runTest(t, integrationTestCase{
 		description: "force namespace",
 		release:     "myapp",
@@ -92,7 +92,7 @@ func TestIntegration(t *testing.T) {
 		},
 	})
 
-	// SAVE_SNAPSHOT=1 go1.20 test -run ^TestIntegration/kube_version_and_api_versions$ ./
+	// SAVE_SNAPSHOT=1 go1.22 test -run ^TestIntegration/kube_version_and_api_versions$ ./
 	runTest(t, integrationTestCase{
 		description: "kube_version_and_api_versions",
 		release:     "vers1",
@@ -107,7 +107,7 @@ func TestIntegration(t *testing.T) {
 		},
 	})
 
-	// SAVE_SNAPSHOT=1 go1.20 test -run ^TestIntegration/disabled_inaccessible_chart_yaml_dep$ ./
+	// SAVE_SNAPSHOT=1 go1.22 test -run ^TestIntegration/disabled_inaccessible_chart_yaml_dep$ ./
 	runTest(t, integrationTestCase{
 		description: "disabled inaccessible chart yaml dep",
 		release:     "inaccessible1",
@@ -131,7 +131,7 @@ func TestIntegration(t *testing.T) {
 	// Local Chart
 	//
 
-	// SAVE_SNAPSHOT=1 go1.20 test -run ^TestIntegration/local_chart_with_adhoc_dependency$ ./
+	// SAVE_SNAPSHOT=1 go1.22 test -run ^TestIntegration/local_chart_with_adhoc_dependency$ ./
 	runTest(t, integrationTestCase{
 		description: "local chart with adhoc dependency",
 		release:     "myapp",
@@ -150,7 +150,7 @@ func TestIntegration(t *testing.T) {
 		},
 	})
 
-	// SAVE_SNAPSHOT=1 go1.20 test -run ^TestIntegration/local_chart_with_slash_at_the_end$ ./
+	// SAVE_SNAPSHOT=1 go1.22 test -run ^TestIntegration/local_chart_with_slash_at_the_end$ ./
 	// Related to https://github.com/helmfile/chartify/pull/13
 	runTest(t, integrationTestCase{
 		description: "local chart with slash at the end",
@@ -173,7 +173,7 @@ func TestIntegration(t *testing.T) {
 		},
 	})
 
-	// SAVE_SNAPSHOT=1 go1.20 test -run ^TestIntegration/local_chart_with_dot_at_the_end$ ./
+	// SAVE_SNAPSHOT=1 go1.22 test -run ^TestIntegration/local_chart_with_dot_at_the_end$ ./
 	// Related to https://github.com/helmfile/chartify/pull/13
 	runTest(t, integrationTestCase{
 		description: "local chart with dot at the end",
@@ -196,7 +196,7 @@ func TestIntegration(t *testing.T) {
 		},
 	})
 
-	// SAVE_SNAPSHOT=1 go1.20 test -run ^TestIntegration/local_chart_with_release_namespace$ ./
+	// SAVE_SNAPSHOT=1 go1.22 test -run ^TestIntegration/local_chart_with_release_namespace$ ./
 	// Related to https://github.com/helmfile/chartify/pull/13
 	runTest(t, integrationTestCase{
 		description: "local chart with release namespace",
@@ -210,7 +210,7 @@ func TestIntegration(t *testing.T) {
 		},
 	})
 
-	// SAVE_SNAPSHOT=1 go1.20 test -run ^TestIntegration/local_chart_with_chart_name_unequal_to_dir_name$ ./
+	// SAVE_SNAPSHOT=1 go1.22 test -run ^TestIntegration/local_chart_with_chart_name_unequal_to_dir_name$ ./
 	// Related to https://github.com/helmfile/chartify/pull/13#issuecomment-1077431214
 	runTest(t, integrationTestCase{
 		description: "local chart with chart name unequal to dir name",
@@ -224,7 +224,7 @@ func TestIntegration(t *testing.T) {
 		},
 	})
 
-	// SAVE_SNAPSHOT=1 go1.20 test -run ^TestIntegration/local_tgz_chart$ ./
+	// SAVE_SNAPSHOT=1 go1.22 test -run ^TestIntegration/local_tgz_chart$ ./
 	runTest(t, integrationTestCase{
 		description: "local tgz chart",
 		release:     "myapp",
@@ -237,7 +237,7 @@ func TestIntegration(t *testing.T) {
 		},
 	})
 
-	// SAVE_SNAPSHOT=1 go1.20 test -run ^TestIntegration/local_chart_with_import_values$ ./
+	// SAVE_SNAPSHOT=1 go1.22 test -run ^TestIntegration/local_chart_with_import_values$ ./
 	runTest(t, integrationTestCase{
 		description: "local_chart_with_import_values",
 		release:     "myapp",
@@ -248,7 +248,7 @@ func TestIntegration(t *testing.T) {
 	// Kubernets Manifests
 	//
 
-	// SAVE_SNAPSHOT=1 go1.20 test -run ^TestIntegration/kube_manifest_with_adhoc_dep$ ./
+	// SAVE_SNAPSHOT=1 go1.22 test -run ^TestIntegration/kube_manifest_with_adhoc_dep$ ./
 	runTest(t, integrationTestCase{
 		description: "kube_manifest_with_adhoc_dep",
 		release:     "myapp",
@@ -267,7 +267,7 @@ func TestIntegration(t *testing.T) {
 		},
 	})
 
-	// SAVE_SNAPSHOT=1 go1.20 test -run ^TestIntegration/kube_manifest_with_patch$ ./
+	// SAVE_SNAPSHOT=1 go1.22 test -run ^TestIntegration/kube_manifest_with_patch$ ./
 	runTest(t, integrationTestCase{
 		description: "kube_manifest_with_patch",
 		release:     "myapp",
@@ -289,7 +289,7 @@ func TestIntegration(t *testing.T) {
 		},
 	})
 
-	// SAVE_SNAPSHOT=1 go1.20 test -run ^TestIntegration/kube_manifest_yml_with_patch$ ./
+	// SAVE_SNAPSHOT=1 go1.22 test -run ^TestIntegration/kube_manifest_yml_with_patch$ ./
 	runTest(t, integrationTestCase{
 		description: "kube_manifest_yml_with_patch",
 		release:     "myapp",
@@ -311,7 +311,7 @@ func TestIntegration(t *testing.T) {
 		},
 	})
 
-	// SAVE_SNAPSHOT=1 go1.20 test -run ^TestIntegration/kube_manifest_transformer_alpha_plugin$ ./
+	// SAVE_SNAPSHOT=1 go1.22 test -run ^TestIntegration/kube_manifest_transformer_alpha_plugin$ ./
 	runTest(t, integrationTestCase{
 		description: "kube_manifest_transformer_alpha_plugin",
 		release:     "myapp",
@@ -408,7 +408,7 @@ func doTest(t *testing.T, tc integrationTestCase) {
 	snapshotFile := filepath.Join("testdata", "integration", "testcases", strings.ReplaceAll(tc.description, " ", "_"), "want")
 
 	// You can update the snapshot by running e.g.:
-	//   SAVE_SNAPSHOT=1 go1.20 test -run ^TestFramework$ ./
+	//   SAVE_SNAPSHOT=1 go1.22 test -run ^TestFramework$ ./
 	if os.Getenv("SAVE_SNAPSHOT") != "" {
 		testcaseDir := filepath.Dir(snapshotFile)
 		err = os.MkdirAll(testcaseDir, 0755)
